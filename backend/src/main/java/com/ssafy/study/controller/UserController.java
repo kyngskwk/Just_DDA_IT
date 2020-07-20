9c20c8c85982fb41cde5236c0791d198aff200d7
Lee Yujin <ajph03102@naver.com>
2020-07-20 22:10:41 +0900

    BE-main create UserController UserController 틀만 만들어 둠 pom.xml 수정 dependency 추가 springfox-swagger2 / springfox-swagger-ui / json / mysql-connector-java / spring-boot-starter-data-jpa / spring-boot-starter-jdbc / validation-api

---
+++ backend/src/main/java/com/ssafy/study/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.ssafy.study.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = { "http://localhost:3000" })
+@RestController
+public class UserController {
+    
+}
