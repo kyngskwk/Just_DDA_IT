c681e8a2873e541465cdb623b56ff4ce75bbd846
yeseul4072 <yeseul4072@naver.com>
2020-07-20 11:49:43 +0900

    modify frontend folder

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,60 +1,35 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/license">License</router-link> |
+      <router-link to="/mystudy">MyStudy</router-link> |
+      <router-link to="/rooms">Rooms</router-link> |
+      <router-link to="/setting">Setting</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
-}
-</script>
-
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 
-h1, h2 {
-  font-weight: normal;
+#nav {
+  padding: 30px;
 }
 
-ul {
-  list-style-type: none;
-  padding: 0;
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
 }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
+#nav a.router-link-exact-active {
   color: #42b983;
 }
 </style>
---
+++ frontend/src/router/index.js
@@ -0,0 +1,46 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import License from '../views/License.vue'
+import MyStudy from '../views/MyStudy.vue'
+import Rooms from '../views/Rooms.vue'
+import Setting from '../views/Setting.vue'
+
+Vue.use(VueRouter)
+
+  const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/license',
+    name: 'License',
+    component: License
+  },
+  {
+    path: '/mystudy',
+    name: 'MyStudy',
+    component: MyStudy
+  },
+  {
+    path: '/rooms',
+    name: 'Rooms',
+    component: Rooms
+  },
+  {
+    path: '/setting',
+    name: 'Setting',
+    component: Setting
+  },
+
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
