98405fa3aad44463c5a206ae6dedb0e2a055f89d
kyngs_kwk <kyngskwk@gmail.com>
2020-07-20 16:27:04 +0900

    FE-roomsindex S03P12A102-17 create index components

---
+++ frontend/src/components/Rooms/RoomCreate.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <button class="btn btn-success">스터디 생성하기</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RoomCreate'
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/src/components/Rooms/RoomListDetail.vue
@@ -0,0 +1,15 @@
+<template>
+  <li>
+      스터디방 
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'RoomListDetail'
+}
+</script>
+
+<style>
+
+</style>
