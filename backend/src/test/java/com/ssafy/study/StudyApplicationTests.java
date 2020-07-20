92e4cef26e4d002618caec530930402cba54f77c
1000min <o_ogog@naver.com>
2020-07-20 10:05:59 +0900

    BE/FE init

---
+++ backend/src/test/java/com/ssafy/study/StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
