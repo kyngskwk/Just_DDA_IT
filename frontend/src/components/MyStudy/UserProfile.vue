6125e4858bd838a2ec6f6d6c72ba55b0a6638e20
yeseul4072 <yeseul4072@naver.com>
2020-07-21 12:21:54 +0900

    FE-userprofile S03P12A102-10 create userprofile component

---
+++ frontend/src/components/MyStudy/UserProfile.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      <h3>유저프로필 컴포넌트</h3>
+  </div>
+</template>
+
+<script>
+export default {
+    name : "UserProfile"
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/src/views/MyStudy.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <UserProfile/>
+  </div>
+</template>
+
+<script>
+import UserProfile from '@/components/MyStudy/UserProfile.vue'
+
+export default {
+    name : "MyStudy",
+    components : {
+        UserProfile,
+    }
+}
+</script>
+
+<style>
+
+</style>
