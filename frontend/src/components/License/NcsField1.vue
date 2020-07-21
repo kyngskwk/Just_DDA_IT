63fc01ed62de85a34c039a1804e88c3adaeeb60a
gandolkim <gandolkim@gmail.com>
2020-07-21 15:30:19 +0900

    FE-ncsfields S01P12A102-19 ncsfield1.vue updated. working on progress.

--- frontend/src/components/License/NcsField1.vue
+++ frontend/src/components/License/NcsField1.vue
@@ -1,139 +1,54 @@
 <template>
-<div class="license-block container">
-      <div class="license-content row">
-        <router-link to="license/ncsfield2" class="col-3 btn btn-white">
-          <span class="btn_go2" @click="fn_clickNcsField('01', '사업관리')">
-            01. 사업관리
-          </span>
-        </router-link>
-        <router-link to="license/ncsfield2" class="col-3 btn btn-white">
-          <span class="btn_go2" @click="fn_clickNcsField('01', '경영 회계 사무')">
-            02. 경영 회계 사무
-          </span>
-        </router-link>
-        <router-link to="license/ncsfield2" class="col-3 btn btn-white">
-          <span class="btn_go2" @click="fn_clickNcsField('01', '금융 보험')">
-            03. 금융 보험
-          </span>
-        </router-link>
-        <router-link to="license/ncsfield2" class="col-3 btn btn-white">
-          <span class="btn_go2" @click="fn_clickNcsField('01', '교육 자연 사회과학')">
-            04. 교육 자연 사회과학
-          </span>
-        </router-link>
-        <router-link to="license/ncsfield2" class="col-3 btn btn-white">
-          <span class="btn_go2" @click="fn_clickNcsField('01', '법률 경찰 소방 교도 국방')">
-            05. 법률 경찰 소방 교도 국방
-          </span>
-        </router-link>
-        <router-link to="license/ncsfield2" class="col-3 btn btn-white">
-          <span class="btn_go2" @click="fn_clickNcsField('01', '보건 의료')">
-            06. 보건 의료
-          </span>
-        </router-link>
-        <router-link to="license/ncsfield2" class="col-3 btn btn-white">
-          <span class="btn_go2" @click="fn_clickNcsField('01', '사회 복지 종교')">
-            07. 사회 복지 종교
-          </span>
-        </router-link>
-        <router-link to="license/ncsfield2" class="col-3 btn btn-white">
-          <span class="btn_go2" @click="fn_clickNcsField('01', '문화 예술 디자인 방송')">
-            08. 문화 예술 디자인 방송
-          </span>
-        </router-link>
-        <router-link to="license/ncsfield2" class="col-3 btn btn-white">
-          <span class="btn_go2" @click="fn_clickNcsField('01', '운전 운송')">
-            09. 운전 운송
-          </span>
-        </router-link>
-        <router-link to="license/ncsfield2" class="col-3 btn btn-white">
-          <span class="btn_go2" @click="fn_clickNcsField('01', '영업판매')">
-            10. 영업판매
-          </span>
-        </router-link>
-        <router-link to="license/ncsfield2" class="col-3 btn btn-white">
-          <span class="btn_go2" @click="fn_clickNcsField('01', '경비 청소')">
-            11. 경비 청소
-          </span>
-        </router-link>
-        <router-link to="license/ncsfield2" class="col-3 btn btn-white">
-          <span class="btn_go2" @click="fn_clickNcsField('01', '이용 숙박 여행 오락 스포츠')">
-            12. 이용 숙박 여행 오락 스포츠
-          </span>
-        </router-link>
-        <router-link to="license/ncsfield2" class="col-3 btn btn-white">
-          <span class="btn_go2" @click="fn_clickNcsField('01', '음식서비스')">
-            13. 음식서비스
-          </span>
-        </router-link>
-        <router-link to="license/ncsfield2" class="col-3 btn btn-white">
-          <span class="btn_go2" @click="fn_clickNcsField('01', '건설')">
-            14. 건설
-          </span>
-        </router-link>
-        <router-link to="license/ncsfield2" class="col-3 btn btn-white">
-          <span class="btn_go2" @click="fn_clickNcsField('01', '기계')">
-            15. 기계
-          </span>
-        </router-link>
-        <router-link to="license/ncsfield2" class="col-3 btn btn-white">
-          <span class="btn_go2" @click="fn_clickNcsField('01', '재료')">
-            16. 재료
-          </span>
-        </router-link>
-        <router-link to="license/ncsfield2" class="col-3 btn btn-white">
-          <span class="btn_go2" @click="fn_clickNcsField('01', '화학')">
-            17. 화학
-          </span>
-        </router-link>
-        <router-link to="license/ncsfield2" class="col-3 btn btn-white">
-          <span class="btn_go2" @click="fn_clickNcsField('01', '섬유 의복')">
-            18. 섬유 의복
-          </span>
-        </router-link>
-        <router-link to="license/ncsfield2" class="col-3 btn btn-white">
-          <span class="btn_go2" @click="fn_clickNcsField('01', '전기 전자')">
-            19. 전기 전자
-          </span>
-        </router-link>
-        <router-link to="license/ncsfield2" class="col-3 btn btn-white">
-          <span class="btn_go2" @click="fn_clickNcsField('01', '정보통신')">
-            20. 정보통신
-          </span>
-        </router-link>
-        <router-link to="license/ncsfield2" class="col-3 btn btn-white">
-          <span class="btn_go2" @click="fn_clickNcsField('01', '식품가공')">
-            21. 식품가공
-          </span>
-        </router-link>
-        <router-link to="license/ncsfield2" class="col-3 btn btn-white">
-          <span class="btn_go2" @click="fn_clickNcsField('01', '인쇄 목재 가구 공예')">
-            22. 인쇄 목재 가구 공예
-          </span>
-        </router-link>
-        <router-link to="license/ncsfield2" class="col-3 btn btn-white">
-          <span class="btn_go2" @click="fn_clickNcsField('01', '환경 에너지 안전')">
-            23. 환경 에너지 안전
-          </span>
-        </router-link>
-        <router-link to="license/ncsfield2" class="col-3 btn btn-white">
-          <span class="btn_go2" @click="fn_clickNcsField('01', '농림어업')">
-            24. 농림어업
-          </span>
-        </router-link>
-      </div>
+  <div class="license-block container">
+    <div 
+    class="license-content row"
+    v-for="ncs_field in ncs_fields"
+    v-bind:key="ncs_field.number"
+    >
+    <router-link to="license/ncsfield2" class="col-3 btn btn-white">
+        {{ ncs_field.number }}. {{ ncs_field.title }}
+    </router-link>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
     name: 'NcsField1',
     props: {
-       
-    },
+   },
     methods: {
         fn_clickNcsField: function() {
-      
+        }
+    },
+    data() {
+        return {
+            ncs_fields: [
+           {number: '01', title: '사업관리' },
+           {number: '02', title: '경영 회계 사무' },
+           {number: '03', title: '금융 보험' },
+           {number: '04', title: '교육 자연 사회과학' },
+           {number: '05', title: '법률 경찰 소방 교도 국방' },
+           {number: '06', title: '보건 의료' },
+           {number: '07', title: '사회 복지 종교' },
+           {number: '08', title: '문화 예술 디자인 방송' },
+           {number: '09', title: '운전 운송' },
+           {number: '10', title: '영업판매' },
+           {number: '11', title: '경비 청소' },
+           {number: '12', title: '이용 숙박 여행 오락 스포츠' },
+           {number: '13', title: '음식서비스' },
+           {number: '14', title: '건설' },
+           {number: '15', title: '기계' },
+           {number: '16', title: '재료' },
+           {number: '17', title: '화학' },
+           {number: '18', title: '섬유 의복' },
+           {number: '19', title: '전기 전자' },
+           {number: '20', title: '정보통신' },
+           {number: '21', title: '식품가공' },
+           {number: '22', title: '인쇄 목재 가구 공예' },
+           {number: '23', title: '환경 에너지 안전' },
+           {number: '24', title: '농림어업' }
+       ]
         }
     }
 }
--- frontend/src/components/License/NcsSearch.vue
+++ frontend/src/components/License/NcsSearch.vue
@@ -1,17 +1,29 @@
 <template>
   <div class="license-search-bar">
-    <label for="license-title">자격증 검색</label>
-    <input type="text" id="license-title">
-    <button>검색</button>
+    <label for="license-title">자격증 검색하기   </label>
+    <input v-model="keyword" type="text" id="license-title" @keyup:enter="searchKeyword">
+    <button @click="searchKeyword">검색</button>
   </div>
 </template>
 
 <script>
 export default {
-
+  name: 'NcsSearch',
+  data() {
+    return {
+      keyword: ''
+    }
+  },
+  methods: {
+    searchKeyword() {
+      console.log(this.keyword)
+    }
+  }
 }
 </script>
 
 <style>
-
+.license-search-bar > label {
+  padding: 4px;
+}
 </style>
