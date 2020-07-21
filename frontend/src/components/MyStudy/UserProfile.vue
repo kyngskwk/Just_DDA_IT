0d8a0e35a4b623589d76ec72e9e04d2f08309338
yeseul4072 <yeseul4072@naver.com>
2020-07-21 16:15:15 +0900

    FE-userprofile S03P12A102-10 working on userprofile

--- frontend/src/components/MyStudy/UserProfile.vue
+++ frontend/src/components/MyStudy/UserProfile.vue
@@ -1,12 +1,29 @@
 <template>
   <div>
       <h3>유저프로필 컴포넌트</h3>
+      <img :src="user.thumbnail">
+      <h4>{{ user.username }}</h4>
+      <button><router-link to="/setting">프로필 편집</router-link></button>
   </div>
 </template>
 
 <script>
+
 export default {
-    name : "UserProfile"
+    name : "UserProfile",
+    props : {
+      user: {
+        type: Object
+      }
+    },
+    data () {
+      return {
+      }
+    },
+    methods : {
+
+      
+    }
 }
 </script>
 
--- frontend/src/views/MyStudy.vue
+++ frontend/src/views/MyStudy.vue
@@ -1,14 +1,27 @@
 <template>
   <div>
-      <UserProfile/>
+      <UserProfile :user="user"/>
   </div>
 </template>
 
 <script>
 import UserProfile from '@/components/MyStudy/UserProfile.vue'
+import axios from 'axios'
 
 export default {
     name : "MyStudy",
+    data() {
+        return {
+            user: {}
+        }
+    },
+    created() {
+        axios.get('http://localhost:3000/member.json')
+        .then(res => {
+            this.user = res.data.data[0]
+            console.log(this.user)
+        })
+    },
     components : {
         UserProfile,
     }
