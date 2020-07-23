60a2ca60e9f9e196e1ab5b7672e1d479f57528b6
kyngs_kwk <kyngskwk@gmail.com>
2020-07-23 14:26:17 +0900

    FE-studylist S03P12A102-37 create studyroon list

--- frontend/src/components/Rooms/RoomList.vue
+++ frontend/src/components/Rooms/RoomList.vue
@@ -1,20 +1,38 @@
 <template>
   <ul>
-    <RoomListDetail class="my-5"/>
+    <RoomListDetail class="my-5" v-for="room in rooms" :key="room.roomId" :room="room"/>
   </ul>
 </template>
 
 <script>
+import axios from 'axios'
 import RoomListDetail from '../Rooms/RoomListDetail.vue'
 
 export default {
   name: 'RoomList',
   components: {
     RoomListDetail
+  },
+  data() {
+    return {
+      rooms: []
+    }
+  },
+  created() {
+    axios.get('http://localhost:3000/studyroom.json')
+    .then(response => {
+      // console.log(response)
+      this.rooms = response.data.data
+    })
+    .catch((error) => {
+      console.log(error);
+    })
   }
 }
 </script>
 
-<style>
-
+<style scoped>
+ul {
+  padding: 0 0 0 0
+}
 </style>
