9ded1e2689028200a0f975eddb7bc22e8ff39861
김진혁 <gandolkim@gmail.com>
2020-07-24 12:10:02 +0900

    FE-licensedetail S03P12A102-40 license detail list and item page added. need a connection  with license.json data

---
+++ frontend/src/components/License/LicenseDetailList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <LicenseDetailListItem />
+  </div>
+  
+</template>
+
+<script>
+import LicenseDetailListItem from './LicenseDetailListItem.vue'
+export default {
+  name: 'LicenseDetailList'
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/src/components/License/LicenseDetailListItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h6>this is LicenseDetailListItem</h6>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LicenseDetailListItem'
+}
+</script>
+
+<style>
+
+</style>
