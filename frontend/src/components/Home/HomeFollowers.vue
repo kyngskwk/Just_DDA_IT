57537a9c55064c58b1201a21182653681657e029
김진혁 <gandolkim@gmail.com>
2020-07-27 13:46:38 +0900

    FE-mainpage S03P12A102-48 Home page created. Banner and Followers are added.

---
+++ frontend/src/components/Home/HomeFollowers.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    파..팔로우..
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeFollowers'
+}
+</script>
+
+<style>
+
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,17 +1,32 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="home-navbar d-flex justify-space-between align-center">
+      <span content=""></span>
+      <a class="mr-4" href="#">로그인</a>
+
+    </div>
+    <hr class="m-0">
+    <HomeFollowers />
+    <hr class="m-0">
+    <HomeBanner />
+
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import HomeBanner from '../components/Home/HomeBanner.vue'
+import HomeFollowers from '../components/Home/HomeFollowers.vue'
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HomeBanner,
+    HomeFollowers
   }
 }
 </script>
+
+<style scoped>
+  
+</style>
