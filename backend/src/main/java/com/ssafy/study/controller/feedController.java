ca87af62e3e365cfe45ca2cc8358375b74aef40b
Lee Yujin <ajph03102@naver.com>
2020-07-28 09:07:34 +0900

    temp

--- backend/src/main/java/com/ssafy/study/controller/feedController.java
+++ backend/src/main/java/com/ssafy/study/controller/feedController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -80,6 +81,16 @@ public class feedController {
 		
 		return response;
 	}
+	
+	@GetMapping("feedListDesc")
+	public Object feedListDesc() {
+		ResponseEntity response = null;
+		BasicResponse result = new BasicResponse();
+		
+		
+		return "";
+	}
+	
 	
 	@PostMapping("/addComment")
 	public Object addComment(@RequestBody Comment comment, HttpSession session) {
