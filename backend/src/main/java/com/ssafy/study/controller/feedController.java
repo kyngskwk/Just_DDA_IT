9c8913d4b245aa2535ca6834d9e39c0d9915a5d7
Lee Yujin <ajph03102@naver.com>
2020-07-28 09:11:30 +0900

    merge..?

--- backend/src/main/java/com/ssafy/study/controller/feedController.java
+++ backend/src/main/java/com/ssafy/study/controller/feedController.java
@@ -10,15 +10,12 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-<<<<<<< HEAD
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-=======
 import org.springframework.web.bind.annotation.*;
->>>>>>> 40eda1d66e0d349856af3bbabab6de015edec7cf
 
 import com.ssafy.study.model.BasicResponse;
 import com.ssafy.study.model.Comment;
