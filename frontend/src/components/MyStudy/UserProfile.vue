5f1c722c1b17c3ae1d31d517f525151cf0746326
yeseul4072 <yeseul4072@naver.com>
2020-07-30 00:40:02 +0900

    BEFE-test follow function fin

--- frontend/src/components/MyStudy/UserProfile.vue
+++ frontend/src/components/MyStudy/UserProfile.vue
@@ -15,7 +15,8 @@
           <v-icon>mdi-pencil</v-icon>
         </v-btn>
       </div>
-      <v-btn v-if="!isSameUser" color="primary" @click="addfollower">follow</v-btn>
+      <v-btn v-if="!isSameUser && !followState" color="primary" @click="follow">follow</v-btn>
+      <v-btn v-if="!isSameUser && followState" color="primary" @click="unfollow">unfollow</v-btn>
     </div>
     <!-- 팔로우/팔로워/좋아요 -->
     <div class="follow d-flex justify-content-around">
@@ -41,7 +42,8 @@ export default {
     data () {
       return {
         "hostUID": this.$route.params.UID,
-        "clientUID" : this.$store.state.member.loginUID,
+        "clientUID" : '1', //this.$store.state.member.loginUID,
+        "followState" : false,
         "followerNums" : '',
         "followingNums" : ''
       }
@@ -51,13 +53,28 @@ export default {
       editProfile() {
         return this.$router.push({ name: 'Setting' })
       },
-      addfollower() {
-        // 호스트 유저의 팔로워에 추가 
+      follow() {
+        // 호스트 유저의 팔로워에 추가 git 
         axios.post('http://localhost:8080/follow', {
           targetid: this.hostUID,
-          uid: "1"
+          uid: this.clientUID
         })
         .then( function() {
+          this.followState=true;
+          console.log();
+        })
+        .finally(function(){
+          console.log(this.hostUID)
+          console.log(this.clientUID)
+        })
+      },
+      unfollow() {
+        axios.post('http://localhost:8080/unfollow', {
+          targetid: this.hostUID,
+          uid: this.clientUID
+        })
+        .then( function() {
+          this.followState=false;
           console.log();
         })
         .finally(function(){
@@ -65,6 +82,39 @@ export default {
           console.log(this.clientUID)
         })
       }
+    },
+    created() {
+      // 팔로우 여부 
+      axios.post('http://localhost:8080/followstate', {
+        targetid: this.hostUID,
+        uid: this.clientUID
+      })
+      .then( res => {
+        this.followState = res.data.object
+        console.log('here')
+        console.log(res.data)
+      })
+      .catch( res => {
+        console.log(res)
+      })
+      .finally(function(){
+        console.log('followcheck().')
+      })
+      // 팔로워 리스트
+      axios.post('http://localhost:8080/getFollower', {
+        targetid: this.hostUID
+      })
+      .then ( res => {
+        console.log('here!!!')
+        console.log(res.data)
+      }),
+      axios.post('http://localhost:8080/getFollowing', {
+        targetid: this.hostUID
+      })
+      .then ( res => {
+        console.log('here@@@')
+        console.log(res.data)
+      })
     },
     computed : {
       isSameUser() {
