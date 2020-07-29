40c75113afeb31e152a6b483c1209ad7e6920c48
Lee Yujin <ajph03102@naver.com>
2020-07-29 18:07:27 +0900

    FS-commenttest updateComment

--- backend/src/main/java/com/ssafy/study/controller/feedController.java
+++ backend/src/main/java/com/ssafy/study/controller/feedController.java
@@ -202,40 +202,45 @@ public class feedController {
 		return response;
 	}
 
-//	@PostMapping("/updateComment")
-//	public Object updateComment(@RequestBody Comment comment, @RequestParam Long feedId, @RequestParam Long UID, HttpSession session) {
-//		ResponseEntity response = null;
-//        BasicResponse result = new BasicResponse();
-//        
-////        Long id = (Long)session.getAttribute("uid");
-//		Optional<Member> member = memberRepo.findById(UID);
-//		Optional<Feed> feed = feedRepo.findById(feedId);
-//		if(!member.isPresent()) {
-//			result.status = false;
-//			result.data = "멤버를 찾을 수 없음.";
-//			return new ResponseEntity<>(result, HttpStatus.FORBIDDEN);
-//		} else if(!feed.isPresent()) {
-//			result.status = false;
-//			result.data = "해당  피드를 찾을 수 없음";
-//			return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
-//		}
-//       
-//		comment.setFeed(feed.get());
-//		comment.setMember(member.get());
-//        commentRepo.save(comment);
-//		member.get().addComment(comment);
-//		feed.get().addComment(comment);
-//		memberRepo.save(member.get());
-//		feedRepo.save(feed.get());
-//		
-//		
-//        result.status = true;
-//		result.data = "success";
-//		
-//		response = new ResponseEntity<>(result, HttpStatus.OK);
-//		
-//		return response;
-//	}
+	@PostMapping("/updateComment")
+	public Object updateComment(@RequestBody Comment comment, @RequestParam Long feedId, @RequestParam Long UID, HttpSession session) {
+		ResponseEntity response = null;
+        BasicResponse result = new BasicResponse();
+        
+//        Long id = (Long)session.getAttribute("uid");
+		Optional<Member> member = memberRepo.findById(UID);
+		Optional<Feed> feed = feedRepo.findById(feedId);
+		Optional<Comment> checkComment = commentRepo.findById(comment.getId());
+		if(!member.isPresent()) {
+			result.status = false;
+			result.data = "멤버를 찾을 수 없음.";
+			return new ResponseEntity<>(result, HttpStatus.FORBIDDEN);
+		} else if(!feed.isPresent()) {
+			result.status = false;
+			result.data = "해당  피드를 찾을 수 없음";
+			return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
+		} else if(!checkComment.isPresent()) {
+			result.status = false;
+			result.data = "해당 댓글을 찾을 수 없음";
+			return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
+		}
+       
+        commentRepo.save(comment);
+		member.get().addComment(comment);
+		feed.get().addComment(comment);
+		memberRepo.save(member.get());
+		feedRepo.save(feed.get());
+		
+        result.status = true;
+		result.data = "success";
+		
+		response = new ResponseEntity<>(result, HttpStatus.OK);
+		
+		return response;
+	}
+	
+	
+	
 	
 	@GetMapping("/getCommentList")
 	public Object getCommentList(@RequestParam Long feedId) {
