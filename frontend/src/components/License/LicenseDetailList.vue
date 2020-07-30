a2aa89a8a56423c82d9f32f6eb417df462dd1e6e
gandolkim <gandolkim@gmail.com>
2020-07-30 16:53:23 +0900

    FE-licenseInfinityScroll S03P12A102-41 work in progress.

--- frontend/src/components/License/LicenseDetailList.vue
+++ frontend/src/components/License/LicenseDetailList.vue
@@ -1,16 +1,47 @@
 <template>
-  <div>
-    <LicenseDetailListItem />
-  </div>
-  
+  <v-data-table
+    :headers="headers"
+    :items="licenseArray"
+    item-key="licenseCode"
+    class="elevation-1"
+  >
+    
+  </v-data-table>
 </template>
 
 <script>
-import LicenseDetailListItem from './LicenseDetailListItem.vue'
 export default {
   name: 'LicenseDetailList',
   components: {
-    LicenseDetailListItem
+  },
+  props: {
+    licenseArray: {
+      type: Array
+    }
+  },
+  watch: {
+    selectedLicenseArray: function() {
+      return this.licenseArray
+    } 
+  },
+  methods: {
+    selectLicense: function(licenseName) {
+      this.$store.state.license.selectedLicense = licenseName
+    }
+  },
+  data: function() {
+    return {
+      headers: [
+        {
+          text: '자격증이름',
+          align: 'start',
+          sortable: false,
+          value: 'licenseName',
+        },
+        { text: '자격증 고유번호', value: 'licenseCode' },
+        { text: '자격증 종류', value: 'licenseSeries' },
+      ],
+    }
   }
 }
 </script>
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -8,6 +8,7 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     license: {
+      selectedLicense: '',
       field1: '',
       field2: '',
       ncs_fields: [
