ed7cc0e1bd631975e99bd6dfa6c9328673fe2bc8
kyngskwk <kyngskwk@gmail.com>
2020-08-05 11:36:41 +0900

    FS-roomcrud create password modal

--- frontend/src/components/Rooms/RoomListDetail.vue
+++ frontend/src/components/Rooms/RoomListDetail.vue
@@ -22,9 +22,33 @@
     </v-list-item>
 
     <v-card-actions>
-      <v-btn v-if="this.room.Private" depressed color="primary" @click="goDetail">🔓둘러보기</v-btn>
-      <v-btn v-if="!this.room.Private" depressed color="primary" @click="goDetail">🔐비밀방</v-btn>
+      <v-btn v-if="!this.room.private" depressed color="primary" @click="goDetail">🔓둘러보기</v-btn>
+      <v-btn v-if="this.room.private" depressed color="primary" @click="goPassword">🔐비밀방</v-btn>
     </v-card-actions>
+
+    <v-dialog v-model="dialog" width="500">
+      <v-card>
+        <v-card-title class="headline blue lighten-2">
+          비밀번호를 적어주세요.
+        </v-card-title>
+
+        <v-card-text class="mt-3">
+          이 방은 비밀방입니다. <br> 방장이 지정한 비밀번호를 적어주세요.
+          <!-- <v-text-field label="Outlined" placeholder="Placeholder" outlined></v-text-field> -->
+
+          <v-text-field :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+            :rules="[rules.required, rules.min]" :type="show1 ? 'text' : 'password'" name="input-10-1"
+            label="비밀번호" hint="방장이 지정한 비밀번호를 적어주세요." counter @click:append="show1 = !show1"></v-text-field>        
+          </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" text @click="dialog = false">I accept</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-card>
 </template>
 
@@ -43,12 +67,26 @@ export default {
       licenseName: this.room.licenseName,
       captainName: this.room.captain.userName,
       hashtag: this.room.roomHashtag,
-      Dday: ''
+      Dday: '',
+      dialog: false,
+      show1: false,
+      show2: true,
+      show3: false,
+      show4: false,
+      password: 'Password',
+      rules: {
+        required: value => !!value || 'Required.',
+        min: v => v.length >= 8 || 'Min 8 characters',
+        emailMatch: () => ('The email and password you entered don\'t match'),
+      }
     }
   },
   methods: {
     goDetail() {
       this.$router.push({name: 'RoomDetail', params: { roomId:this.room.roomId }})
+    },
+    goPassword() {
+      this.dialog = true
     }
   },
   created() {
