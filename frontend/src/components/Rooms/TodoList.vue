8da3fcc1e809581d242e44255f099d8df6d6f19b
kyngskwk <kyngskwk@gmail.com>
2020-08-09 17:24:38 +0900

    FS-calendar search

--- frontend/src/components/Rooms/TodoList.vue
+++ frontend/src/components/Rooms/TodoList.vue
@@ -1,9 +1,8 @@
 <template>
 <div class="card-body">
   <div>
-    <h4 class="mb-4 indigo--text">Today's TodoList : {{ todothings.length }}</h4>
-    <v-checkbox v-for="todo in todothings" :key="todo.id" 
-    :label="todo.todoContent" :value="todo.todoContent" class="my-0"></v-checkbox>
+    <h4 class="mb-4 indigo--text">Today's TodoList : {{ todaythings.length }}</h4>
+    <p v-for="todo in todaythings" :key="todo.id" >{{ todo.todoContent }}</p>
   </div>
 </div>
 </template>
@@ -13,7 +12,7 @@
 export default {
   name: 'TodoList',
   props: {
-    dateForStudyrooms: {
+    todaythings: {
       type: Array
     }
   },
@@ -21,35 +20,7 @@ export default {
     return {
       todothings: [],
     }
-  },
-  created() {
-    // 형식 바꾸는 거
-    function leadingZeros(n, digits) {
-      var zero = '';
-      n = n.toString();
-
-      if (n.length < digits) {
-        for (i = 0; i < digits - n.length; i++)
-          zero += '0';
-      }
-      return zero + n;
-    }
-
-    var now = new Date();
-    // 형식 바꾸는거
-    var nowtime = 
-      leadingZeros(now.getFullYear(), 4) + '-' +
-      leadingZeros(now.getMonth() + 1, 2) + '-' +
-      leadingZeros(now.getDate(), 2);
-
-    // console.log(nowtime)
-    for(var i=0; i < this.dateForStudyrooms.length; i++) {
-      if (this.dateForStudyrooms[i].todoDate == nowtime) {
-        this.todothings.push(this.dateForStudyrooms[i])
-        // this.tasks.push({isChecked: this.dateForStudyrooms[i].isChecked, text: this.dateForStudyrooms[i].todoContent})
-      }
-    }
-  },
+  }
 }
 </script>
 
