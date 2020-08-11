8b165e123795e547108dea27e66ced9bed1b2a20
gandolkim <gandolkim@gmail.com>
2020-08-12 01:35:02 +0900

    FE-licenseHighChart S03P13A103-15 highchart added. need a connection with real-data

--- frontend/src/components/License/LicenseResult.vue
+++ frontend/src/components/License/LicenseResult.vue
@@ -4,11 +4,10 @@
       <v-icon dark>mdi-arrow-left</v-icon>
     </v-btn>
     <div class="text-center">
-      <h1 class="mb-4">검색 결과</h1>
-      <span v-if="!isFieldSelected">검색하신 단어: {{ keyword }}</span>
-      <h3 v-if="isFieldSelected">선택하신 분야: {{ field1 }}, {{ field2 }}</h3>
+      <h1 class="result-h1 mb-4">검색 결과</h1>
+      <h3 class="result-text" v-if="!isFieldSelected">검색하신 단어: {{ keyword }}</h3>
+      <h3 class="result-text" v-if="isFieldSelected">선택하신 분야: {{ field1 }}, {{ field2 }}</h3>
       <hr />
-      <h5>검색된 자격증으로는</h5>
 
       <LicenseResultList v-if="!isFieldSelected" :licenseArray="licenseArray" />
       <LicenseResultList v-if="isFieldSelected" :licenseArray="license_based_on_fields" />
@@ -81,7 +80,13 @@ export default {
 
 
 <style scoped>
-[v-clock] {
-  display: none;
+.result-h1 {
+  color: #fd462e ;
+  font-family: 'Black Han Sans', sans-serif;
+}
+
+.result-text {
+  color: #fd462e ;
+  font-family: 'Black Han Sans', sans-serif;
 }
 </style>
--- frontend/src/components/License/LicenseResultDetail.vue
+++ frontend/src/components/License/LicenseResultDetail.vue
@@ -15,9 +15,9 @@
 
       <!-- 유저가 가지고 있는 자격증들을 체크 가능 -->
       <div class="my-2">
-        <v-btn class="mx-1" small>찜하기</v-btn>
-        <v-btn class="mx-1" small color="primary">준비중!</v-btn>
-        <v-btn class="mx-1" small color="error">이미있어요!</v-btn>
+        <v-btn :disabled="isTodo" @click="addTodo" class="mx-1" small>찜하기</v-btn>
+        <v-btn :disabled="isDoing" @click="addDoing" class="mx-1" small color="primary">준비중!</v-btn>
+        <v-btn :disabled="isPass" @click="addPass" class="mx-1" small color="error">이미있어요!</v-btn>
       </div>
       
       <!-- 자격증에 대한 상세정보 탭 -->
@@ -29,22 +29,35 @@
         <li class="mb-2">진로 및 전망: {{ selectedLicenseInfo.career }}</li>
       </ul>
 
-      <!-- <highcharts :options="chartOptions" /> -->
+      <highcharts :options="chartOptions"></highcharts>
+
+
       <LicenseReview :licenseInfo="selectedLicense" />
     </div>
   </div>
 </template>
 
 <script>
+import Highcharts from 'highcharts'
+import VariablePie from 'highcharts/modules/variable-pie'
+import {Chart} from 'highcharts-vue'
 import axios from 'axios'
 import LicenseReview from "./LicenseReview.vue";
+
+VariablePie(Highcharts)
 
 export default {
   name: "LicenseResultDetail",
   components: {
     LicenseReview,
+    highcharts: Chart
+  },
+  created: function() {
+
   },
   mounted: function () {
+    ////////////////////////////////////
+    // selectedLicenseInfo에 해당자격증의 디테일을 넣음
     const licenseSeries = this.selectedLicense.licenseSeries
     const LICENSE_SERIES_URL = `field_info_0${licenseSeries}_output.json`
     axios.get('http://localhost:3000/license/' + LICENSE_SERIES_URL)
@@ -61,6 +74,33 @@ export default {
         
       })
       .catch( err => console.log(err.message))
+
+    //////////////////////////////////////
+    // 유저가 가지고 있는 라이센스 정보를 가져옴
+      console.log('created start')
+      axios.get('http://localhost:8080/license/getMyLicense', {
+      params: {
+        UID: this.hostID
+      }
+    })
+    .then(res => {
+      const licenses = res.data.object
+      // 지금 자격증이 mylicense에 있는지 확인 => 버튼 비/활성화
+      licenses.forEach( obj => {
+        console.log('selectedLicenseCode', this.selectedLicense.licenseCode, 'obj license code', obj.license.licenseCode)
+        if(this.selectedLicense.licenseCode === obj.license.licenseCode) {
+          if(obj.licenseStatus === 'todo'){
+            this.isTodo = true
+          } else if(obj.licenseStatus === 'doing') {
+            this.isDoing = true
+          } else {
+            this.isPass = true
+          }
+        }
+      })
+    })
+    .catch( res => {console.log(res.message)})
+
   },
   computed: {
     isEmptyObject() {
@@ -68,26 +108,129 @@ export default {
       return Object.keys(params).length === 0 && params.constructor === Object;
     },
   },
+  watch: {
+    'passLicenses': function(){
+      this.$emit("cntPass", this.passLicenses.length)
+    }
+  },
   methods: {
-    
+    addTodo() {
+      
+    },
+    addDoing() {
+
+    },
+    addPass() {
+
+    }
   },
   data: function () {
     return {
+      // 유저에게서 자격증 취득 현황을 받아오기 위한 변수들
+      hostID: 1,
+      passLicenses: {
+        type: Array
+      },
+      todoLicenses: {
+        type: Array
+      },
+      isTodo: false,
+      isDoing: false,
+      isPass: false,
+
+      // 선택된 자격증에 대한 정보
       selectedLicenseInfo: {
         type: Object
       },
       selectedLicense: this.$store.state.license.selectedLicense,
+      
       chartOptions: {
-        sertries: [
-          {
-            data: [1, 2, 3]
-          }
-        ]
+        chart: {
+          type: 'variablepie'
+        },
+        title: {
+          text: '아시나요? 합격자의 68퍼센트는 대학생 때 이자격증을 취득했습니다.'
+        },
+        tooltip: {
+          headerFormat: '',
+          pointFormat: '<span style="color:{point.color}">\u25CF</span> <b> {point.name}</b><br/>' +
+            '전체 수험생 중 비율: <b>{point.y} % </b><br/> ' +
+            '응시자 수: <b>{point.z}</b><br/>'
+        },
+        series: [{
+          minPointSize: 10,
+          innerSize: '20%',
+          zMin: 0,
+          name: 'countries',
+          data: [{
+              name: '미상',
+              y: 6,
+              z: 92
+            }, {
+              name: '전문대 재학',
+              y: 8,
+              z: 55
+            }, {
+              name: '전문대 졸업',
+              y: 13,
+              z: 124
+            }, {
+              name: '대학 재학',
+              y: 58,
+              z: 188
+            }, {
+              name: '대학 졸업',
+              y: 21,
+              z: 137
+            }, {
+              name: '기타',
+              y: 4,
+              z: 21
+            }
+          ]
+        }]   
       }
     };
   },
 };
 </script>
 
-<style>
+<style scoped>
+#container {
+	height: 500px;
+}
+
+.highcharts-figure, .highcharts-data-table table {
+  min-width: 320px; 
+  max-width: 700px;
+  margin: 1em auto;
+}
+
+.highcharts-data-table table {
+	font-family: Verdana, sans-serif;
+	border-collapse: collapse;
+	border: 1px solid #EBEBEB;
+	margin: 10px auto;
+	text-align: center;
+	width: 100%;
+	max-width: 500px;
+}
+.highcharts-data-table caption {
+  padding: 1em 0;
+  font-size: 1.2em;
+  color: #555;
+}
+.highcharts-data-table th {
+	font-weight: 600;
+  padding: 0.5em;
+}
+.highcharts-data-table td, .highcharts-data-table th, .highcharts-data-table caption {
+  padding: 0.5em;
+}
+.highcharts-data-table thead tr, .highcharts-data-table tr:nth-child(even) {
+  background: #f8f8f8;
+}
+.highcharts-data-table tr:hover {
+  background: #f1f7ff;
+}
 </style>
