eae99485a8aeea58b76cd85f1f0e36a7e5254026
Lee Yujin <ajph03102@naver.com>
2020-08-12 17:42:01 +0900

    BE-feedranking S03P13A102-37 likeRanking

--- backend/src/main/java/com/ssafy/study/controller/feedController.java
+++ backend/src/main/java/com/ssafy/study/controller/feedController.java
@@ -19,6 +19,8 @@ import javax.servlet.http.HttpSession;
 
 import com.ssafy.study.dto.feedDTO;
 import com.ssafy.study.dto.feedEditDTO;
+import com.ssafy.study.dto.likeCountDTO;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -469,6 +471,42 @@ public class feedController {
 		result.data = "success";
 		result.object=feeds;
 
+		response = new ResponseEntity<>(result, HttpStatus.OK);
+
+		return response;
+	}
+	
+	public void name() {
+		Date date = new Date();
+		Collection<Like> likelist = likeRepo.findAll();
+		List<Feed> list = likelist.stream().filter(Like->Like.getFeed().getRegistTime().before(date)).map(Like::getFeed).collect(Collectors.toList());
+		Map<Feed,Integer> map = new HashMap<Feed,Integer>();
+		for(Feed like : list) {
+			map.put(like, map.getOrDefault(like, 0)+1);
+		} 
+		Set<Map.Entry<Feed, Integer>>entries = map.entrySet();
+		entries.stream().sorted().collect(Collectors.toList());
+		
+	}
+
+
+
+	
+	@GetMapping("/likeRanking")
+	public Object likeRanking() {
+		ResponseEntity response = null;
+		BasicResponse result = new BasicResponse();
+		Collection<Feed> feeds = feedRepo.findAllByRegistTimeGreaterThan(new Date(System.currentTimeMillis() - 1000 * 60 * 60 * 24));
+		List<likeCountDTO> counts = new ArrayList<likeCountDTO>();
+		for (Feed feed : feeds) {
+			counts.add(new likeCountDTO(feed,likeRepo.countByFeed(feed)));
+			
+		}
+		
+		
+		result.status = true;
+		result.data = "success";
+//		result.object=;
 		response = new ResponseEntity<>(result, HttpStatus.OK);
 
 		return response;
