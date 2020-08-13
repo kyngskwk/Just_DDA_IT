a60c16f99f97370af70e40b758ae6dc23bc2aa12
kyngskwk <kyngskwk@gmail.com>
2020-08-13 11:03:48 +0900

    FS-searchname front

--- frontend/src/components/Rooms/RoomList.vue
+++ frontend/src/components/Rooms/RoomList.vue
@@ -4,7 +4,12 @@
     <span class="pink--text">{{this.content[0].searchThing}}</span> 을 <span class="pink--text">{{this.content[0].category}}</span>(으)로 검색한 결과입니다.
   </div>
   <v-row class="background">
-    <RoomListDetail class="my-5" v-for="room in rooms" :key="room.id" :room="room"/>
+    <div v-if="this.content.length != 0 && this.content[0].category == '이름'">
+      <UserListItem/>
+    </div>
+    <div v-else>
+      <RoomListDetail class="my-5" v-for="room in rooms" :key="room.id" :room="room"/>
+    </div>
   </v-row>
 </div>
 </template>
@@ -12,11 +17,13 @@
 <script>
 import axios from 'axios'
 import RoomListDetail from '../Rooms/RoomListDetail.vue'
+import UserListItem from '../Rooms/UserListItem.vue'
 
 export default {
   name: 'RoomList',
   components: {
-    RoomListDetail
+    RoomListDetail,
+    UserListItem
   },
   data() {
     return {
@@ -68,7 +75,7 @@ export default {
           // this.$emit('search-end')
         })
       }
-      else {
+      else if (this.content[0].category == '방장') {
         axios.get(`http://${this.$store.state.address}:8080/study/findStudyroomByCaptain`, {
           params: {
             captainName:this.content[0].searchThing
@@ -81,6 +88,16 @@ export default {
         })
         .catch((error) => {
           console.log(error);
+        })
+      }
+      else {
+        axios.get(`http://${this.$store.state.address}:8080/study/searchMembers`, {
+          params: {
+            name:this.content[0].searchThing
+          }
+        })
+        .then(response => {
+          console.log(response)
         })
       }
     }
---
+++ frontend/src/components/Rooms/UserListItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    유저
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'UserListItem'
+
+}
+</script>
+
+<style>
+
+</style>
