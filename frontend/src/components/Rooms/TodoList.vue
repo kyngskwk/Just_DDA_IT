8dd26516ee1c9e11e361505adcd20f086ce0a843
kyngskwk <kyngskwk@gmail.com>
2020-08-14 12:32:54 +0900

    FS-calendar todo front

--- frontend/src/components/Rooms/TodoList.vue
+++ frontend/src/components/Rooms/TodoList.vue
@@ -2,10 +2,13 @@
 <div class="card-body">
   <div>
     <h4 class="mb-4 font_e font-weight-bold" style="color:#fd462e;">
-      <span class="font-weight-light mr-2">Today's</span>TodoList <v-btn fab>{{ todaythings.length }}</v-btn>
+      <span class="font-weight-light mr-2">Today's</span>TodoList 
+       <v-avatar color="#fd462e" outline size="40">
+          <span class="white--text headline">{{ todaythings.length }}</span>
+        </v-avatar>
     </h4>
     <div v-if="this.in == true">
-      <v-checkbox  v-for="todo in todaythings" :key="todo.id" v-model="todo.checked"
+      <v-checkbox class="font_k" v-for="todo in todaythings" :key="todo.id" v-model="todo.checked"
       value :label="todo.dateForStudyroom.todoContent" color="red" @click="$emit('checked', todo)"></v-checkbox>
     </div>
     <p v-else v-for="todo in todaythings" :key="todo.id" >{{ todo.dateForStudyroom.todoContent }}</p>
@@ -42,6 +45,8 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+label {
+  padding-top: 20px;
+}
 </style>
