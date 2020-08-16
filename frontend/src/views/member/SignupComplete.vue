c7b678d6675ccf256c764826437f97fbd8d3f164
yeseul4072 <yeseul4072@naver.com>
2020-08-16 13:54:27 +0900

    FE-mystudy-ui

--- frontend/src/views/member/SignupComplete.vue
+++ frontend/src/views/member/SignupComplete.vue
@@ -1,8 +1,13 @@
 <template>
-  <v-container style="max-width:350px">
-    <h4>회원가입이 완료되었습니다.</h4>
-    <v-btn @click="$router.push({name: 'Home'})" block>메인화면</v-btn>
-    <v-btn @click="$router.push({name: 'Login'})" block color="primary">로그인</v-btn>
+  <v-container fill-height class="d-flex flex-column align-center mt-15" style="max-width:350px">
+    <span style="color: #fd462e;">
+      <i class="far fa-check-circle fa-3x"></i>
+    </span>
+    <h5 class="font_k mt-5" style="font-weight:bold">회원가입이 완료되었습니다!</h5>
+    <div class="d-flex flex-column justify-center mt-10" style="width:80%">
+      <v-btn @click="$router.push({name: 'Home'})" rounded outlined block color="#fd462e">메인화면</v-btn>
+      <v-btn @click="$router.push({name: 'Login'})" rounded dark block color="#fd462e" class="mt-2">로그인</v-btn>
+    </div>
   </v-container>
 </template>
 
