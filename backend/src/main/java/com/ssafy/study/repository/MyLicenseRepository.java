3810c89fdccc7600ee6dd547a9cfa08372614f2d
gandolkim <gandolkim@gmail.com>
2020-08-19 20:27:54 +0900

    FE-licenseFirstCheck detail info emphasized. Todo&Doing&Pass are fully functional.

--- backend/src/main/java/com/ssafy/study/repository/MyLicenseRepository.java
+++ backend/src/main/java/com/ssafy/study/repository/MyLicenseRepository.java
@@ -12,6 +12,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface MyLicenseRepository extends JpaRepository<MyLicense,Long> {
 	Optional<MyLicense> findById(Long id);
 	Optional<MyLicense> findByMemberAndLicense(Member member, License license);
+	Optional<MyLicense> findByMemberAndLicenseAndLicenseStatus(Member member, License license,String licenseStatus);
 	Collection<MyLicense> findByLicenseStatus(String licenseStatus);
 	Collection<MyLicense> findAllByMember(Member member);
 	Collection<MyLicense> findAllByLicense(License license);
