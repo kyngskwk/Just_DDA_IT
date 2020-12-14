8693a75410dc5cc25446e11f2f4d0119c9f96fe5
김진혁 <gandolkim@gmail.com>
2020-12-14 15:05:39 +0900

    eslint applied

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -6,35 +6,32 @@
         <a v-show="isAuthenticated" class="mr-4 position-right" href="#">로그인</a>
         <a v-show="!isAuthenticated" class="mr-4 position-right" href="#">로그아웃</a> -->
       </div>
-
     </div>
-    <hr class="m-0">
+    <hr class="m-0" />
     <HomeFollowers />
-    <hr class="m-0">
+    <hr class="m-0" />
     <HomeBanner />
-    <HomeStudyList/>
-
-
+    <HomeStudyList />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HomeBanner from '../components/Home/HomeBanner.vue'
-import HomeFollowers from '../components/Home/HomeFollowers.vue'
-import HomeStudyList from '../components/Home/HomeStudyList.vue'
+import HomeBanner from "../components/Home/HomeBanner.vue";
+import HomeFollowers from "../components/Home/HomeFollowers.vue";
+import HomeStudyList from "../components/Home/HomeStudyList.vue";
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
     HomeBanner,
     HomeFollowers,
-    HomeStudyList
+    HomeStudyList,
   },
   methods: {
     // changeUserState: function() {
     //   this.isAuthenticated = !this.isAuthenticated
     //   if (this.isAuthenticated)
-    //     this.StudyListMessage = "당신을 위한 추천 스터디방" 
+    //     this.StudyListMessage = "당신을 위한 추천 스터디방"
     //   else
     //     this.StudyListMessage = "이번주 인기 스터디방"
     // }
@@ -45,9 +42,8 @@ export default {
   //     StudyListMessage: "이번주의 인기 스터디방"
   //   }
   // }
-}
+};
 </script>
 
 <style scoped>
-  
 </style>
